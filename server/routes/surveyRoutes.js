e00b8024d315dd039eff5a91b04cd231fda44b13
ubhagat <ubhagat@umich.edu>
2020-06-14 20:19:47 +0530

    database and webhook synced

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -2,17 +2,22 @@ const _ = require('lodash');
 const { Path } = require('path-parser');
 const { URL } = require('url');
 
+// middleware
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
-const mongoose = require('mongoose');
 
+// db
+const mongoose = require('mongoose');
+const Survey = mongoose.model('surveys');
+
+// sendGrid stuff
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
-const Survey = mongoose.model('surveys');
+
 
 module.exports = app => {
 
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/:surveys/:choice', (req, res) => {
         res.send('Thanks for your response');
     })
 
@@ -46,18 +51,39 @@ module.exports = app => {
     app.post('/api/surveys/webhooks', (req, res) => {
         const p = new Path('/api/surveys/:surveyId/:choice');
 
-        const events = _.chain(req.body)
-            .map((event) => {
-                const match = p.test(new URL(event.url).pathname);
+        _.chain(req.body)
+            .map(({ email, url }) => {
+                const match = p.test(new URL(url).pathname);
                 if (match) {
-                    return { email: event.email, surveyId: match.surveyId, choice: match.choice };
+                    return { email: email, surveyId: match.surveyId, choice: match.choice };
                 }
             })
             .compact()
             .uniqBy('email', 'surveyId')
+            .each(({ surveyId, email, choice }) => {
+                Survey.updateOne({
+                    _id: surveyId,
+                    recipients: {
+                        $elemMatch: { email: email, responded: false}
+                    }
+                }, {
+                    $inc: { [choice]: 1},
+                    $set: { 'recipients.$.responded': true },
+                    lastResponded: new Date()
+                }).exec();
+            })
             .value();
 
-        console.log(events);
+        // this chain 
+        // 1. map -> maps all the events and then basically extracts the needed parts of the whole req.body (path parser, lodash, url specific)
+        // 2. compact -> Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+        // 3. uniqby -> gets the unique value only (incase user clicks twice)
+        // 4. each -> iterates through the events and updates the database accordingly. First, we find a matching record
+        // then we update the model using $inc and $set. exec just executes it
+        // 5. value -> gives us the updated value
+
+        // _id -> because mongoose specific ID
+
         res.send({});  // to avoid duplicate and make sure sendgrid thinks everything ok
     });
 };
